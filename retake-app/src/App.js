2472658a3ebdcead33ef2bd450eab137a1c63031
Will Stenzel <wstenzel@stu.naperville203.org>
2024-01-30 14:01:15 -0600

    sidebar tutorial 1

--- retake-app/src/App.js
+++ retake-app/src/App.js
@@ -18,7 +18,14 @@ function App() {
           
         </a>
       </header>
+      <div className="App" id="outer-container">
+      <div id="page-wrap">
+        <h1>Cool Restaurant</h1>
+        <h2>Check out our offerings in the sidebar!</h2>
+      </div>
     </div>
+    </div>
+    
   );
 }
 
