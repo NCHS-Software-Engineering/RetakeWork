b4d53732c05daf36e8f57368a58d26652164a7aa
Will Stenzel <wstenzel@stu.naperville203.org>
2024-02-07 14:23:00 -0600

    Routes added, everything broken

--- retake-app/src/pages/logIn.js
+++ retake-app/src/pages/logIn.js
@@ -1,17 +1,30 @@
-
-import './login.css';
-
-import React from "react";
- 
+import React from 'react';
+import './Login.css';
 const Login = () => {
     return (
-        <div>
-            <h1>
-                Hello
-            </h1>
+        <div className='wrapper'>
+            <form action="">
+                <h1>Login</h1>
+                <div className='input-box'>
+                    <input type="text" placeholder='Username' required/>
+                    </div>
+                    <div className='input-box'>
+                    <input type="text" placeholder='Username' required/>
+                    </div>
+
+                    <div className="remember-forget">
+                        <label><input type ="checkbox"/>Remember me</label>
+                        <a href ='#' >Forget password</a>
+                    </div>
+
+                    <button type='submit'>Login</button>
+
+                    <div className="register-link">
+                        <p>click make account <a href ="#">Register</a></p>
+</div>
+                    </form>
         </div>
     );
 };
- 
+
 export default Login;
-  
