ad9f9bde415a81e98cc6c69f69acae6e35003ddb
a1exung <110874630+a1exung@users.noreply.github.com>
2024-02-16 13:38:38 -0600

    changes

---
+++ retake-app/src/Sidebar.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import './Sidebar.css';
+import Select from "react-select";
+
+
+
+export default props => {
+  const [options1, setOptions] = useState([]);
+
+  
+  return (
+    <Menu>
+      <div className="mt-auto m-auto w-50">
+<Select options = {options1} autoFocus={true} />
+</div>
+
+    </Menu>
+  );
+};
+
