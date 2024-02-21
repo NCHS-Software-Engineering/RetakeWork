fd67ef8835415fc0aef029f8f604a4acc2e74b48
Will Stenzel <wstenzel@stu.naperville203.org>
2024-02-21 14:07:51 -0600

    Dummy Data

--- retake-app/src/pages/Sidebar.js
+++ retake-app/src/pages/Sidebar.js
@@ -19,42 +19,49 @@ import {
 export default props => {
 
   const [isOpenableClasses, setSelectedClasses] = useState(true);
-
+  const [testOptions, setTests] = useState([]);
   const handleChange = (selectedOption) => {
 
     if (selectedOption.value === "login") {
       window.location.href = "./login";
-      setSelectedClasses(true);
+    
     }
     if (selectedOption.value === "default") {
       window.location.href = "./";
-      setSelectedClasses(true);
+     
     }
     if (selectedOption.value === "signup") {
       window.location.href = "./signup";
-      setSelectedClasses(true);
+   
     }
     if (selectedOption.value === "home") {
-      setSelectedClasses(false);
+     ;
     }
 
   };
 
   const handleChangeClasses = (selectedOption) => {
 
-    if (selectedOption.value === "login") {
-      window.location.href = "./login";
-      setSelectedClasses(true);
+    if (selectedOption.value === "prog1") {
+      setTests([
+      { value: 'test1', label: 'Chapter 1 Test' },
+      { value: 'test2', label: 'Chapter 2 Test' },
+      { value: 'test3', label: 'Chapter 3 Test' },
+      { value: 'test4', label: 'Chapter 4 Test' }]);
+      setSelectedClasses(false);
     }
-    if (selectedOption.value === "default") {
-      window.location.href = "./";
-      setSelectedClasses(true);
+    if (selectedOption.value === "APCS") {
+      setTests([
+      { value: 'test5', label: 'Unit 1: Primitive Types Test' },
+      { value: 'test6', label: 'Unit 5: Writing Classes Test' },
+      { value: 'test7', label: 'Unit 10: Recursion Test' }]);
+      setSelectedClasses(false);
     }
-    if (selectedOption.value === "signup") {
-      window.location.href = "./signup";
-      setSelectedClasses(true);
-    }
-    if (selectedOption.value === "home") {
+    if (selectedOption.value === "SE") {
+      setTests([
+      { value: 'test9', label: 'Chapter 15 Exam' },
+      { value: 'test10', label: 'Maze Lab' },
+      { value: 'test11', label: 'Chapter 17 Test' }]);
       setSelectedClasses(false);
     }
 
@@ -92,14 +99,17 @@ export default props => {
     { value: 'APCS', label: 'AP Computer Science A' },
     { value: 'SE', label: 'Software Engineering' },];
 
+  
+
   return (
     <Menu>
       <div className="mt-auto m-auto w-50">
         <Select placeholder="Pages" options = {options1} autoFocus={true} onChange={handleChange} styles={customStyles}/>
         <p></p>
-        <Select placeholder="Classes" options = {classOptions} autoFocus={true} onChange={handleChangeClasses} styles={customStyles} isDisabled={isOpenableClasses}/>
+        <Select placeholder="Classes" options = {classOptions} autoFocus={true} onChange={handleChangeClasses} styles={customStyles}/>
         <p></p>
-        <Select placeholder="Test" options = {options1} autoFocus={true} onChange={handleChange} styles={customStyles}/>
+        <Select placeholder="Test" options = {testOptions} autoFocus={true} onChange={handleChange} styles={customStyles}  isDisabled={isOpenableClasses}/>
+        <p></p>
       </div>
       <div className = "signout">
         <Link to="/"><button class="signoutbutton">Sign Out</button></Link>
