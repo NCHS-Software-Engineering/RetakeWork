06063d6cc1828d626a949cc4ed2cbcb59fd3f3a9
Will Stenzel <wstenzel@stu.naperville203.org>
2024-02-26 13:39:06 -0600

    signup working

--- retake-app/src/pages/Signup.js
+++ retake-app/src/pages/Signup.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import './Login.css';
+import './Signup.css';
 import {
-    BrowserRouter as Link,
+    Link as Link,
   } from "react-router-dom";
 
 
@@ -9,30 +9,30 @@ const Login = () => {
     return (
         <div className='wrapper'>
             <form action="">
-                <h1>Sign Up</h1>
+            <h1>Sign Up</h1>
 
-                <div className='input-box'>
-                    <input type="text" placeholder='Username' required />
-                </div>
+            <div className='input-box'>
+    <input type="text" placeholder='Username' required />
+</div>
 
-                <div className='input-box'>
-                    <input type="text" placeholder='Password' required />
-                </div>
+<div className='input-box'>
+    <input type="text" placeholder='Password' required />
+</div>
 
-                <div className='input-box'>
-                    <input type="text" placeholder='Confirm password' required />
-                </div>
+<div className='input-box'>
+    <input type="text" placeholder='Confirm password' required />
+</div>
 
-               {/*  <div className="remember-forget">
-                    <label><input type="checkbox" />Remember me   </label>
-                    <a href='#' >   Forget password</a>
-                </div> */}
+{/*  <div className="remember-forget">
+    <label><input type="checkbox" />Remember me   </label>
+    <a href='#' >   Forget password</a>
+</div> */}
 
-                <Link to="/"><button type='submit'>Signup</button></Link>
+<Link to="/"><button type='submit'>Signup</button></Link>
 
-                <div className="register-link">
-                    <p>Click here to <Link to="/login">login</Link></p>
-                </div>
+<div className="register-link">
+    <p>Click here to <Link to="/login">login</Link></p>
+</div>
             </form>
         </div>
     );
