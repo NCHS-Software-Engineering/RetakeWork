6aee2823d7e1f9b59f37e091b7e7d1c5ed9040e1
Will Stenzel <wstenzel@stu.naperville203.org>
2024-03-04 13:46:09 -0600

    button formatting better

--- retake-app/src/pages/Signup.js
+++ retake-app/src/pages/Signup.js
@@ -2,37 +2,37 @@ import React from 'react';
 import './Signup.css';
 import {
     Link as Link,
-  } from "react-router-dom";
+} from "react-router-dom";
 
 
 const Login = () => {
     return (
         <div className='wrapper'>
             <form action="">
-            <h1>Sign Up</h1>
+                <h1>Sign Up</h1>
 
-            <div className='input-box'>
-    <input type="text" placeholder='Username' required />
-</div>
+                <div className='input-box'>
+                    <input type="text" placeholder='Username' required />
+                </div>
 
-<div className='input-box'>
-    <input type="text" placeholder='Password' required />
-</div>
+                <div className='input-box'>
+                    <input type="text" placeholder='Password' required />
+                </div>
 
-<div className='input-box'>
-    <input type="text" placeholder='Confirm password' required />
-</div>
+                <div className='input-box'>
+                    <input type="text" placeholder='Confirm password' required />
+                </div>
 
-{/*  <div className="remember-forget">
+                {/*  <div className="remember-forget">
     <label><input type="checkbox" />Remember me   </label>
     <a href='#' >   Forget password</a>
 </div> */}
 
-<Link to="/login"><button className = 'signButtonUp' type='submit'>Signup</button></Link>
+                <Link to="/home"><button className='signButtonUp' type='submit'>Signup</button></Link>
 
-<div className="register-link">
-    <p>Click here to <Link to="/login">login</Link></p>
-</div>
+                <div className="register-link">
+                    <p>Click here to <Link to="/login">login</Link></p>
+                </div>
             </form>
         </div>
     );
